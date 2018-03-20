782173123ac94d179f8686edd7bd696925963950
Tan Avery <altan@ualberta.ca>
2018-03-20 11:15:53 -0600

    added a boolean draft attribute as well as a datefield 'publish' attribute to the Post model

--- posts/urls.py
+++ posts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 	path('', views.post_list, name = 'list'),
 	path('create/', views.post_create),
 	path('<int:id>/', views.post_detail, name = 'detail'),
-	path('update/<int:id>/edit', views.post_update, name = 'update'),
+	path('<int:id>/edit', views.post_update, name = 'update'),
 	path('delete/<int:id>', views.post_delete),
 
 ]
